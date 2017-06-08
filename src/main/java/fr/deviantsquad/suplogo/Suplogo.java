a1be0651ac4badb85dcaa3104aa9d7b44cb6022c
robin4002 <robin4002@gmail.com>
2017-06-08 14:36:52 +0200

    add gradle

---
+++ src/main/java/fr/deviantsquad/suplogo/Suplogo.java
@@ -0,0 +1,63 @@
+package fr.deviantsquad.suplogo;
+
+import fr.deviantsquad.suplogo.cursor.Cursor;
+import fr.deviantsquad.suplogo.interpreter.Interpreter;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Suplogo extends Application
+{
+    @Override
+    public void start(Stage primaryStage)
+    {
+
+        // Add the zones
+        BorderPane root = new BorderPane();
+        StackPane center = new StackPane();
+
+        Scene scene = new Scene(root, 800, 880);
+
+        scene.getStylesheets().add("fr/deviantsquad/suplogo/css/style.css");
+
+        // Create the drawing canvas
+        Canvas drawingCanvas = new Canvas(800, 800);
+        // Create the cursor canvas
+        Canvas cursorCanvas = new Canvas(800, 800);
+
+        // Create the input of commands
+        final TextField input = new TextField();
+
+        // Add the canvas to center
+        center.getChildren().add(drawingCanvas);
+        center.getChildren().add(cursorCanvas);
+
+        // Add the canvas and input to the stage
+        root.setBottom(input);
+        root.setCenter(center);
+
+        // Create the interpreter to get and execute commands from the user
+        Interpreter interpreter = new Interpreter(primaryStage, new Cursor(400.0, 400.0), drawingCanvas, cursorCanvas, input);
+
+        // Set the windows parameters
+        primaryStage.setMinHeight(880);
+        primaryStage.setMinWidth(800);
+        primaryStage.setTitle("SupLogo");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+        interpreter.refresh();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
+}
---
+++ src/main/java/fr/deviantsquad/suplogo/cursor/Cursor.java
@@ -0,0 +1,84 @@
+package fr.deviantsquad.suplogo.cursor;
+
+import static java.lang.Math.abs;
+
+public class Cursor {
+    private double x;
+    private double y;
+    private int angle;
+    private String color;
+    private Boolean hidden;
+    private Boolean down;
+
+    /**
+    * Default Cursor constructor
+    */
+    public Cursor(double x, double y) {
+    	this.x = x;
+    	this.y = y;
+    	this.angle = 90; // Default angle (down)
+    	this.color = "#000"; // Default color;
+    	this.hidden = false;
+    	this.down = true;
+    }
+
+    public double getX() {
+    	return this.x;
+    }
+
+    /*
+    private void setX(int x) {
+    	this.x = x;
+    }
+    */
+
+    public void moveX(double x) {
+        this.x += x;
+    }
+
+    public double getY() {
+    	return this.y;
+    }
+
+    /*
+    private void setY(int y) {
+    	this.y = y;
+    }
+    */
+
+    public void moveY(double y) {
+        this.y += y;
+    }
+
+    public int getAngle() {
+    	return this.angle;
+    }
+
+    public void turn(int angle) {
+    	this.angle = (this.angle + angle) % 360;
+    }
+
+    public String getColor() {
+    	return this.color;
+    }
+
+    public void setColor(String color) {
+    	this.color = color;
+    }
+
+    public Boolean isHidden() {
+    	return this.hidden;
+    }
+
+    public void setHide(Boolean hidden) {
+        this.hidden = hidden;
+    }
+
+    public Boolean isDown() {
+    	return this.down;
+    }
+
+    public void setDown(Boolean down) {
+        this.down = down;
+    }
+}
